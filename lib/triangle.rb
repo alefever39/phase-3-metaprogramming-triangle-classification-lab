32b778ec85d8571b36b95e08f1e8c352c81f6fb5
Allie LeFever <alefever39@gmail.com>
2022-06-28 13:53:25 -0600

    commit tree e69ad7970abced86127e2357d8951764041da49c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
+require "pry"
+
 class Triangle
-  # write code here
+  def initialize(side_1, side_2, side_3)
+    @sides = [side_1, side_2, side_3]
+  end
+
+  def kind
+    sorted_sides = @sides.sort { |a, b| a <=> b }
+    if (sorted_sides.min(2).sum <= sorted_sides.max || sorted_sides.min <= 0)
+      raise TriangleError
+    elsif (sorted_sides[0] == sorted_sides[1])
+      (sorted_sides[1] == sorted_sides[2]) ? :equilateral : :isosceles
+    elsif (sorted_sides[1] == sorted_sides[2])
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
